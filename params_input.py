50e86312391467769a407f9b00dff0c110247f0e
Daria Mihaila <dariam2001@yahoo.com>
2025-01-07 21:11:03 +0100

    Delete params_input.py

--- params_input.py
+++
@@ -1,7 +0,0 @@
-'''File containing the input parameters for the simulation -- rate, frequency, amplitude and duration of the possonian sine input signal'''
-
-rate = [10000.0, 10000.0] # mean firing rate of the input generator -- NOTE: should it perhaps not be the same to ensure unbiased results based on this.
-amplitude = [5000.0, 7500.0]
-frequency = [10.0, 20]
-
-applied_input = 1000.0 # time of learning
